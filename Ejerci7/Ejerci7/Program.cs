5b0fd877c5ead02830f377d28c1d6908cf09ffd1
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-23 20:21:22 -0400

    7 y 8

---
+++ Ejerci7/Ejerci7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] lista = { 5, 10, 15, 20, 25 };
+
+        Console.Write("Ingrese un número : ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        bool encontrado = EstaEnLista(lista, numero);
+
+        if (encontrado)
+            Console.WriteLine("esta.");
+        else
+            Console.WriteLine("no esta.");
+    }
+
+    static bool EstaEnLista(int[] arreglo, int valor)
+    {
+        foreach (int elemento in arreglo)
+        {
+            if (elemento == valor)
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ Ejerci8/Ejerci8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una cadena de texto: ");
+        string texto = Console.ReadLine();
+
+        int longitud = CalcularLongitud(texto);
+
+        Console.WriteLine("La longitud de la cadena es: " + longitud);
+    }
+
+    static int CalcularLongitud(string cadena)
+    {
+        return cadena.Length;
+    }
+}
