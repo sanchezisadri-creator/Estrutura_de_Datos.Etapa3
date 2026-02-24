de109b9de160c9d57c3aa0cf88963d2e673ea9c8
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-23 20:28:05 -0400

    subiendo el primero de la segunda pagina

---
+++ Ejerci1_segunda_pagina/Ejerci1_segunda_pagina/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Persona
+{
+    public string nombre;
+
+    public void Saludar()
+    {
+        Console.WriteLine("Hola, soy " + nombre);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona persona1 = new Persona();
+        persona1.nombre = "Isa";
+
+        persona1.Saludar();
+    }
+}
