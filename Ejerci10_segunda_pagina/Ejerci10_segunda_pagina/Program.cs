0086e1d4dfa5ba918b376afae6459f9f6636f733
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-23 21:01:26 -0400

    Ultimo de la segunda pagina

---
+++ Ejerci10_segunda_pagina/Ejerci10_segunda_pagina/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var numeros = (2, 4, 2, 6, 2, 8);
+
+        Console.Write("Ingrese el número a buscar: ");
+        int buscar = Convert.ToInt32(Console.ReadLine());
+
+        int contador = 0;
+
+        if (numeros.Item1 == buscar) contador++;
+        if (numeros.Item2 == buscar) contador++;
+        if (numeros.Item3 == buscar) contador++;
+        if (numeros.Item4 == buscar) contador++;
+        if (numeros.Item5 == buscar) contador++;
+        if (numeros.Item6 == buscar) contador++;
+
+        Console.WriteLine("El número aparece " + contador + " veces.");
+    }
+}
