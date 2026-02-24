b84dfe13485f589db268eb6970e1393908beca14
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-23 20:37:31 -0400

    subien3 y 4 por ahora esta exotico

---
+++ Ejerci2_segunda_pagina/Ejerci2_segunda_pagina/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Perro
+{
+    public string raza;
+
+    public void MostrarRaza()
+    {
+        Console.WriteLine("La raza del perro es: " + raza);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Perro miPerro = new Perro();
+        miPerro.raza = "Labrador";
+
+        miPerro.MostrarRaza();
+    }
+}
---
+++ Ejerci3_segunda_pagina/Ejerci3_segunda_pagina/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Coche
+{
+    public string color;
+
+    public string ObtenerColor()
+    {
+        return color;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Coche miCoche = new Coche();
+        miCoche.color = "Rojo";
+
+        string resultado = miCoche.ObtenerColor();
+
+        Console.WriteLine("El color del coche es: " + resultado);
+    }
+}
